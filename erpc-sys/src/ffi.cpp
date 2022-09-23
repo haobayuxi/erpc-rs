beb53aad99b9e5b071c068ceca587dacd3eeceda
wuhao2016 <whaohit@gmail.com>
2022-09-23 15:09:54 +0800

    update variable name

--- erpc-sys/src/ffi.cpp
+++ erpc-sys/src/ffi.cpp
@@ -1,14 +1,16 @@
+#include <junction/ConcurrentMap_Leapfrog.h>
+#include <signal.h>
+
+#include <cstring>
+
 #include "rpc.h"
 #include "rpc_types.h"
 #include "transport_impl/eth_common.h"
 #include "util/latency.h"
 #include "util/numautils.h"
-#include <cstring>
-#include <junction/ConcurrentMap_Leapfrog.h>
-#include <signal.h>
 
 class MsgBuffers {
-public:
+ public:
   erpc::MsgBuffer req_msgbuf;
   erpc::MsgBuffer resp_msgbuf;
 };
@@ -16,7 +18,7 @@ public:
 typedef junction::ConcurrentMap_Leapfrog<size_t, MsgBuffers *> ConcurrentMap;
 
 class AppContext {
-public:
+ public:
   erpc::Rpc<erpc::CTransport> *rpc = nullptr;
   int session_num = 0;
   ConcurrentMap msgbufs;
@@ -50,7 +52,7 @@ void erpc_nexus_destroy(erpc::Nexus *nexus) {
 
 void erpc_nexus_register_req_func(erpc::Nexus *nexus, uint8_t req_type,
                                   erpc::erpc_req_func_t req_func,
-                                  uint8_t req_func_type) { // ReqFuncType
+                                  uint8_t req_func_type) {  // ReqFuncType
   nexus->register_req_func(req_type, req_func,
                            erpc::ReqFuncType(req_func_type));
 }
@@ -78,7 +80,7 @@ erpc::Rpc<erpc::CTransport> *erpc_rpc_new(erpc::Nexus *nexus,
                                           uint8_t phy_port) {
   auto ret = new erpc::Rpc<erpc::CTransport>(
       nexus, static_cast<void *>(context), rpc_id, sm_handler, phy_port);
-  ret->retry_connect_on_invalid_rpc_id = true;
+  ret->retry_connect_on_invalid_rpc_id_ = true;
   context->rpc = ret;
   return ret;
 }
@@ -93,8 +95,8 @@ void erpc_rpc_destroy(erpc::Rpc<erpc::CTransport> *rpc) {
 int erpc_connect_session(AppContext *context, const char *server_uri,
                          uint8_t rem_rpc_id) {
   int session_num = context->rpc->create_session(server_uri, rem_rpc_id);
-  //erpc::rt_assert(session_num >= 0, "Failed to create session");
-  // printf("session_num %d\n", session_num);
+  // erpc::rt_assert(session_num >= 0, "Failed to create session");
+  //  printf("session_num %d\n", session_num);
   context->session_num = session_num;
   return session_num;
 }
@@ -104,18 +106,18 @@ bool erpc_rpc_is_connected(erpc::Rpc<erpc::CTransport> *rpc, int session_num) {
 }
 
 void erpc_run_event_loop_once(erpc::Rpc<erpc::CTransport> *rpc) {
-  rpc->run_event_loop_once(); // size_t timeout_ms
+  rpc->run_event_loop_once();  // size_t timeout_ms
 }
 
 void erpc_rpc_run_event_loop(erpc::Rpc<erpc::CTransport> *rpc,
                              size_t timeout_ms) {
-  rpc->run_event_loop(timeout_ms); // size_t timeout_ms
+  rpc->run_event_loop(timeout_ms);  // size_t timeout_ms
 }
 
 uint8_t *erpc_get_req_msgbuf(erpc::ReqHandle *req_handle, size_t &data_size) {
   auto msgbuf = req_handle->get_req_msgbuf();
   data_size = msgbuf->get_data_size();
-  return msgbuf->buf;
+  return msgbuf->buf_;
 }
 
 void erpc_enqueue_request(AppContext *context, erpc::Rpc<erpc::CTransport> *rpc,
@@ -125,16 +127,16 @@ void erpc_enqueue_request(AppContext *context, erpc::Rpc<erpc::CTransport> *rpc,
                           size_t cont_etid) {
   auto buffs = context->alloc_msg_buffer_or_die(tag, data_size, 1024);
 
-  memcpy(buffs->req_msgbuf.buf, data, data_size);
+  memcpy(buffs->req_msgbuf.buf_, data, data_size);
 
   rpc->enqueue_request(session_num, 1, &buffs->req_msgbuf, &buffs->resp_msgbuf,
-                       cont_func, reinterpret_cast<void *>(tag)); // nullptr
+                       cont_func, reinterpret_cast<void *>(tag));  // nullptr
 }
 
 void erpc_enqueue_response(erpc::Rpc<erpc::CTransport> *rpc,
                            erpc::ReqHandle *req_handle, const uint8_t *data,
                            size_t data_size) {
-  auto &resp = req_handle->pre_resp_msgbuf;
+  auto &resp = req_handle->pre_resp_msgbuf_;
   rpc->resize_msg_buffer(&resp, data_size);
   memcpy(resp.buf, data, data_size);
 
@@ -159,11 +161,11 @@ void erpc_msgbuffs_destroy(MsgBuffers *buffs) {
 
 uint8_t *erpc_msgbuffs_req_msgbuf(MsgBuffers *buffs, size_t &data_size) {
   data_size = buffs->req_msgbuf.get_data_size();
-  return buffs->req_msgbuf.buf;
+  return buffs->req_msgbuf.buf_;
 }
 
 uint8_t *erpc_msgbuffs_resp_msgbuf(MsgBuffers *buffs, size_t &data_size) {
   data_size = buffs->resp_msgbuf.get_data_size();
-  return buffs->resp_msgbuf.buf;
+  return buffs->resp_msgbuf.buf_;
 }
 }
